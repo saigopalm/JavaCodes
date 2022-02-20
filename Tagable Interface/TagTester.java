9f5d265ddcf75bb1753bc5920a01791b5943602f
Udvas <uacharjee14@gmail.com>
2022-02-20 12:14:29 +0530

    Made changes to TagTester

--- Tagable Interface/TagTester.java
+++ Tagable Interface/TagTester.java
@@ -1,16 +1,20 @@
+import java.util.Scanner;
 public class TagTester{
 public static void main(String args[]){
 String productCode = Validator.getString("Enter the product code: ");
 Product p = ProductDB.getProduct(productCode);
 String tag;
 String op;
+//dont want to make changes to Validator but want to read the entire line for tag
+Scanner sc = new Scanner(System.in);
 if(p==null){//prnting appropriate message when product not found
 System.out.println("No product match the product Id");
 return;
 }
 do{
-tag = Validator.getString("Enter Tag: ");
-p.setTag(tag);
+System.out.print("Enter tag: ");
+tag = sc.nextLine();
+p.setTag(tag);//setting tag
 do{
 op =Validator.getString("continue(y/n)?");//whether user wants to continue
 }while(!(op.equalsIgnoreCase("y")||op.equalsIgnoreCase("n")));
