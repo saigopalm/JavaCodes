b2a381c9e4fd453589beb06b9c5550d2f2baa8cc
Deepes777 <deepesgp@gmail.com>
2022-02-22 08:07:54 +0530

    new files

---
+++ Deepesh/Arrays/Product.java
@@ -0,0 +1,72 @@
+import java.text.NumberFormat;
+
+public class Product
+{
+    private String code;
+    private String description;
+    private double price;
+    private static int count = 0;
+
+    public Product()
+    {
+        code = "";
+        description = "";
+        price = 0;
+        count++;
+    }
+    
+    public Product(String c, String d, double pri) {
+        this();
+        code = c;
+        description = d;
+        price = pri;
+    }
+
+    public void setCode(String code)
+    {
+        this.code = code;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public static int getCount() {
+        return count; 
+    }
+
+
+    public String getFormattedPrice()
+    {
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        return currency.format(price);
+    }
+
+    public String toString()
+    {
+        return " Code:        " + code + "\n" +
+               " Description: " + description + "\n" +
+               " Price:       " + this.getFormattedPrice() + "\n";
+    }
+
+}
