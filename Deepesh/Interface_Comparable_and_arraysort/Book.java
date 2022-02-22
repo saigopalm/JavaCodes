0ed7cbb69e8d6a8273afb3f6248788c6fc14fd9f
Deepes777 <deepesgp@gmail.com>
2022-02-22 08:09:16 +0530

    new files

---
+++ Deepesh/Interface_Comparable_and_arraysort/Book.java
@@ -0,0 +1,26 @@
+public class Book extends Product {
+    private String author;
+	public Book() {
+		author = "";
+	}
+
+    public Book(String c, String des, double pri, String a) {
+        super(c, des, pri);
+        author = a;
+    }
+    
+	public void setAuthor(String s) {
+		author = s;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}	
+
+	public String toString()
+    {
+        return  super.toString() +
+		" Author:      " + author + "\n";
+    }
+
+}
