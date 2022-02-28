dab990f53a3e583dcbf7ebde59f48c8369269f2b
Udvas <uacharjee14@gmail.com>
2022-02-28 11:38:41 +0530

    added classpath

---
+++ Udvas/Classpath/SSSIHL/javasource/main/Tagable.java
@@ -0,0 +1,13 @@
+
+/**
+ *To implement get Tag and set Tag
+ * @author Udvas Acharjee
+ * @version 20-02-2022
+ */
+
+public interface Tagable
+{
+    
+    void setTag(String s);
+    String getTag();
+}
